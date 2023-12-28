41fddea295671b114fb55806ffffc57eec1922c3
Luan Souza de Almeida <72097035+luanzsouza@users.noreply.github.com>
2023-12-28 11:03:14 -0300

    feat: primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <main>
+     <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              width={100}
+              height={24}
+              priority
+            />
+    </main>
+  )
+}
