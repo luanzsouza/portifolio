a7cf09d1fd72cca36db2f4fd7fca8efac94fd4e8
luan.almeida <luan.almeida@trizy.app>
2024-01-04 16:14:19 -0300

    efetuado a alteracao para a aparecer no gh pages

--- next.config.js
+++ next.config.js
@@ -2,3 +2,10 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+const prefix = isProd ? "/portifolio" : "";
+
+module.exports = {
+  basePath: prefix,
+  assetPrefix: prefix,
+};
--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -1,7 +1,7 @@
 import { GitHubIcon } from "../icons/github-icon";
 import { InstaIcon } from "../icons/insta-icon";
 import { LinkedInIcon } from "../icons/linkedin-icon";
-import { TwitterIcon } from "../icons/twitter-icon";
+
 
 import "./social-btns.scss"
 
@@ -15,7 +15,7 @@ export function SocialBtns(){
             <a href="https://www.linkedin.com/in/luan-souza-de-almeida-531093169/">
                 <LinkedInIcon/>
             </a>
-            <a href="https://https://github.com/luanzsouza">
+            <a href="https://github.com/luanzsouza/">
                 <GitHubIcon/>
             </a>
             
